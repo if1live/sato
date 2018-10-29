0a9c5246ecceabc3789e808f3afee9430ab18a9f
if1live <libsora25@gmail.com>
2018-10-29 11:52:10 +0900

    initial commit

---
+++ server/webpack.config.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const webpack = require('webpack');
+
+const distPath = path.resolve(__dirname, './dist');
+
+const env = process.env.NODE_ENV !== 'production' ? 'development' : 'production';
+
+module.exports = {
+  'entry': path.resolve(__dirname, 'src', 'index.ts'),
+  'output': {
+    'path': distPath,
+    'filename': 'main.js',
+  },
+  'module': {
+    'rules': [
+      {
+        'test': /\.tsx?$/,
+        'use': [
+          'ts-loader',
+        ],
+      },
+    ],
+  },
+  'plugins': [
+    new webpack.ProgressPlugin(),
+  ],
+  'target': 'node',
+  'node': {
+    '__dirname': true,
+  },
+  'externals': [
+  ],
+  'devtool': 'source-map',
+  'resolve': {
+    'extensions': [
+      '.ts',
+      '.tsx',
+      '.js',
+      '.json',
+    ],
+    'alias': {
+      '@src': path.resolve('./src/'),
+    },
+  },
+  'mode': env,
+};
